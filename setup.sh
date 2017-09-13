f0830b3fb59eeed07503b5edba449980a71032da
quangthanh010290 <thanh@autonomous.nyc>
2017-09-13 00:21:20 -0700

    add setup file

---
+++ setup.sh
@@ -0,0 +1,4 @@
+sudo apt-get install python-gobject bluez bluez-tools bluez-firmware python-bluez python-dev python-pip
+sudo pip install evdev 
+sudo apt-get install git-core -y 
+sudo apt-get install tmux -y
