1d9baefd18052196a0fa3067b59589bf7c400459
quangthanh010290 <thanh@autonomous.nyc>
2017-09-18 12:36:19 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 sudo apt-get install python-gobject bluez bluez-tools bluez-firmware python-bluez python-dev python-pip
 sudo pip install evdev 
+sudo apt-get install python-dbus  -y
+sudo apt-get install python-gtk2 -y
 sudo apt-get install git-core -y 
 sudo apt-get install tmux -y
 cd dbus
