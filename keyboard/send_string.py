881ae6e548e1b9c8176e14a8c2ae755057ea4fdb
thanh.le <thanh.le@veriksystems.com>
2020-04-23 14:24:56 +0700

    fix issue on send_string.py

--- keyboard/send_string.py
+++ keyboard/send_string.py
@@ -11,75 +11,69 @@ import keymap
 
 
 class BtkStringClient():
-
-    """simple client of the Bluetooth Keyboard Emulator
-    writes a string to the BTK Keyboard
-    https://thanhle.me/2020/02/18/make-raspberry-pi3-as-an-emulator-bluetooth-keyboard/
-    """
-
 	#constants
 	KEY_DOWN_TIME=0.01
 	KEY_DELAY=0.1
 
-		def __init__(self):
+	def __init__(self):
 
-			#the structure for a bt keyboard input report (size is 10 bytes)
+		#the structure for a bt keyboard input report (size is 10 bytes)
 
-			self.state=[
-					0xA1, #this is an input report
-					0x01, #Usage report = Keyboard
-					#Bit array for Modifier keys
-					[0, #Right GUI - Windows Key
-					 0, #Right ALT
-					 0, #Right Shift
-					 0, #Right Control
-					 0, #Left GUI
-					 0, #Left ALT
-					 0, #Left Shift
-					 0],    #Left Control
-					0x00,   #Vendor reserved
-					0x00,   #rest is space for 6 keys
-					0x00,
-					0x00,
-					0x00,
-					0x00,
-					0x00]
+		self.state=[
+			0xA1, #this is an input report
+			0x01, #Usage report = Keyboard
+			#Bit array for Modifier keys
+			[0, #Right GUI - Windows Key
+				0, #Right ALT
+				0, #Right Shift
+				0, #Right Control
+				0, #Left GUI
+				0, #Left ALT
+				0, #Left Shift
+				0],    #Left Control
+			0x00,   #Vendor reserved
+			0x00,   #rest is space for 6 keys
+			0x00,
+			0x00,
+			0x00,
+			0x00,
+			0x00]
 
 
-			self.scancodes={
-				" ": "KEY_SPACE"
-			}
+		self.scancodes={
+			" ": "KEY_SPACE"
+		}
 
-			#connect with the Bluetooth keyboard server
-			print "setting up DBus Client"
+		#connect with the Bluetooth keyboard server
+		print "setting up DBus Client"
 
-			self.bus = dbus.SystemBus()
-			self.btkservice = self.bus.get_object('org.yaptb.btkbservice','/org/yaptb/btkbservice')
-			self.iface = dbus.Interface(self.btkservice,'org.yaptb.btkbservice')
+		self.bus = dbus.SystemBus()
+		self.btkservice = self.bus.get_object('org.yaptb.btkbservice','/org/yaptb/btkbservice')
+		self.iface = dbus.Interface(self.btkservice,'org.yaptb.btkbservice')
 
-		def send_key_state(self):
+	def send_key_state(self):
 
-			"""sends a single frame of the current key state to the emulator server"""
+		"""sends a single frame of the current key state to the emulator server"""
 
-			bin_str=""
-			element=self.state[2]
-			for bit in element:
-				bin_str += str(bit)
-			self.iface.send_keys(int(bin_str,2),self.state[4:10]  )
+		bin_str=""
+		element=self.state[2]
+		for bit in element:
+			bin_str += str(bit)
+		self.iface.send_keys(int(bin_str,2),self.state[4:10]  )
 
-		def send_key_down(self, scancode):
+	def send_key_down(self, scancode):
 
-			"""sends a key down event to the server"""
+		"""sends a key down event to the server"""
 
-			self.state[4]=scancode
-			self.send_key_state()
+		self.state[4]=scancode
+		self.send_key_state()
 
-		def send_key_up(self):
+	def send_key_up(self):
 
-			"""sends a key up event to the server"""
+		"""sends a key up event to the server"""
 
-			self.state[4]=0
-			self.send_key_state()
+		self.state[4]=0
+		self.send_key_state()
 
 	def send_string(self, string_to_send):
 
