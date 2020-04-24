57c52fb278c6eace58812b843d3c1e44137e9151
thanh.le <thanh.le@veriksystems.com>
2020-04-24 13:47:13 +0700

    fix boot error

--- boot.sh
+++ boot.sh
@@ -10,7 +10,7 @@ sudo hciconfig hcio up
 export C_PATH=$(pwd)
 #Create Tmux session
 tmux has-session -t  mlabviet
-if [ $? != 0 ]; then
+if [ $? != 0 ] ; then
     tmux new-session -s mlabviet -n os -d
     tmux split-window -h -t mlabviet
     tmux split-window -v -t mlabviet:os.0
