132b42c9f0cc7211c9884cf8a2239a043646baae
thanh.le <thanh.le@veriksystems.com>
2020-04-24 13:48:46 +0700

    update

--- boot.sh
+++ boot.sh
@@ -9,14 +9,14 @@ sudo hciconfig hcio up
 #Get current Path
 export C_PATH=$(pwd)
 #Create Tmux session
-tmux has-session -t  mlabviet
+tmux has-session -t  thanhle
 if [ $? != 0 ] ; then
-    tmux new-session -s mlabviet -n os -d
-    tmux split-window -h -t mlabviet
-    tmux split-window -v -t mlabviet:os.0
-    tmux split-window -v -t mlabviet:os.1
-    tmux send-keys -t mlabviet:os.0 'cd $C_PATH && sudo /usr/sbin/bluetoothd --nodetach --debug -p time ' C-m
-    tmux send-keys -t mlabviet:os.1 'cd $C_PATH/server && sudo python btk_server.py ' C-m
-    tmux send-keys -t mlabviet:os.2 'cd $C_PATH && sudo /usr/bin/bluetoothctl' C-m
-    tmux send-keys -t mlabviet:os.3 'cd $C_PATH/keyboard/ && sleep 5 && sudo python kb_client.py' C-m
+    tmux new-session -s thanhle -n os -d
+    tmux split-window -h -t thanhle
+    tmux split-window -v -t thanhle:os.0
+    tmux split-window -v -t thanhle:os.1
+    tmux send-keys -t thanhle:os.0 'cd $C_PATH && sudo /usr/sbin/bluetoothd --nodetach --debug -p time ' C-m
+    tmux send-keys -t thanhle:os.1 'cd $C_PATH/server && sudo python btk_server.py ' C-m
+    tmux send-keys -t thanhle:os.2 'cd $C_PATH && sudo /usr/bin/bluetoothctl' C-m
+    tmux send-keys -t thanhle:os.3 'cd $C_PATH/keyboard/ && sleep 5 && sudo python kb_client.py' C-m
 fi
