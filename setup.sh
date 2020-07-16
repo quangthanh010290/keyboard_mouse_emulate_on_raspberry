8701c19b8191f198cedbbefea663d480f3ab08f2
quangthanh010290 <thanh.le@veriksystems.com>
2020-07-16 11:11:22 +0700

    cleanup setup

--- setup.sh
+++ setup.sh
@@ -1,9 +1,8 @@
-sudo apt-get install python-gobject bluez bluez-tools
+sudo apt-get install bluez bluez-tools
 sudo apt-get install bluez-firmware python-bluez python-dev python-pip -y
 sudo pip install evdev
 sudo apt install git python3-dbus python3-pyudev python3-evdev
 sudo apt-get install python-dbus  -y
-sudo apt-get install python-gtk2 -y
 sudo apt-get install git-core -y
 sudo apt-get install tmux -y
 sudo cp dbus/org.thanhle.btkbservice.conf /etc/dbus-1/system.d
