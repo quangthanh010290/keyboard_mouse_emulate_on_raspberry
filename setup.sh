38f3b71f386645469e56e0ce3d6c330bfceaf10e
quangthanh010290 <quangthanh010290@gmail.com>
2020-08-12 19:09:53 +0700

    remove package git-core in setup script

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@ sudo apt-get install bluez-firmware python-bluez python-dev python-pip -y
 sudo pip install evdev
 sudo apt install git python3-dbus python3-pyudev python3-evdev
 sudo apt-get install python-dbus  -y
-sudo apt-get install git-core -y
 sudo apt-get install tmux -y
 sudo cp dbus/org.thanhle.btkbservice.conf /etc/dbus-1/system.d
 sudo cp /lib/systemd/system/bluetooth.service ./bluetooth.service.bk
