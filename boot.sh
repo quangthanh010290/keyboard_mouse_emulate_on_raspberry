24d410337039800652b960faac2adbb01c956c07
quangthanh010290 <thanh.le@veriksystems.com>
2020-12-15 17:54:55 +0700

    update boot.sh

--- boot.sh
+++ boot.sh
@@ -8,11 +8,28 @@ sudo /etc/init.d/bluetooth start
 ./updateName.sh ThanhLe_Keyboard_Mouse
 #Get current Path
 export C_PATH=$(pwd)
-#Create Tmux session
-tmux has-session -t  thanhle
-if [ $? != 0 ] ; then
-    tmux new-session -s thanhle -n os -d
+
+setupApplication()
+{
+    tmux new-session -s thanhle -n pi_bluetooth -d
     tmux split-window -h -t thanhle
-    tmux split-window -h -t thanhle
-    tmux send-keys -t 0 'cd $C_PATH/server && sudo ./btk_server.py ' C-m
+    tmux split-window -v -t thanhle
+    tmux send-keys -t thanhle:pi_bluetooth.0 'cd $C_PATH/server && reset && sudo ./btk_server.py ' C-m
+    tmux send-keys -t thanhle:pi_bluetooth.1 'cd $C_PATH/mouse  && reset ' C-m
+    tmux send-keys -t thanhle:pi_bluetooth.2 'cd $C_PATH/keyboard  && reset ' C-m
+}
+
+STATUS=$(tmux ls 2>&1)
+
+echo "STATUS: ${STATUS}"
+
+if [ "${STATUS}" = "no server running on /tmp/tmux-1000/default" ] ; then
+    echo "no tmux instance"
+    setupApplication
+else
+    STATUS=$(tmux has-session -t  thanhle 2>&1)
+    if [ "${STATUS}" = "can't find session thanhle" ] ; then
+        echo "no session thanhle"
+        setupApplication
+    fi
 fi
