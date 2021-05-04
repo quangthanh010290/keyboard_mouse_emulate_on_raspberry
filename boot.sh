32f20dc2d3addab0ecc7e07405f03a16bf49c9a6
Simon Cheng <simoncheng559@gmail.com>
2021-05-04 10:26:16 -0400

    fix case where the tmux server has not been created yet
    the double slash in `/tmp//tmux-1000/default` happens when `tmux ls` is first run

--- boot.sh
+++ boot.sh
@@ -23,7 +23,7 @@ STATUS=$(tmux ls 2>&1)
 
 echo "STATUS: ${STATUS}"
 
-if [ "${STATUS}" = "no server running on /tmp/tmux-1000/default" ] ; then
+if [ "${STATUS}" = "no server running on /tmp/tmux-1000/default" ] || [ "${STATUS}" = "error connecting to /tmp/tmux-1000/default (No such file or directory)" ] || [ "${STATUS}" = "error connecting to /tmp//tmux-1000/default (No such file or directory)" ] ; then
     echo "no tmux instance"
     setupApplication
 else
