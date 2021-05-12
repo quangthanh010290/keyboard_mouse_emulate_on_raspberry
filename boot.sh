57f991d57edad272a8648e3dcaa5aa8233b4a54c
quangthanh010290 <quangthanh010290@gmail.com>
2021-05-12 10:11:34 +0100

    clean up

--- boot.sh
+++ boot.sh
@@ -9,15 +9,7 @@ sudo /etc/init.d/bluetooth start
 #Get current Path
 export C_PATH=$(pwd)
 
-setupApplication()
-{
-    tmux new-session -s thanhle -n pi_bluetooth -d
-    tmux split-window -h -t thanhle
-    tmux split-window -v -t thanhle
-    tmux send-keys -t thanhle:pi_bluetooth.0 'cd $C_PATH/server && reset && sudo ./btk_server.py ' C-m
-    tmux send-keys -t thanhle:pi_bluetooth.1 'cd $C_PATH/mouse  && reset ' C-m
-    tmux send-keys -t thanhle:pi_bluetooth.2 'cd $C_PATH/keyboard  && reset ' C-m
-}
+tmux kill-window -t thanhle:app >/dev/null 2>&1
 
 [ ! -z "$(tmux has-session -t thanhle 2>&1)" ] && tmux new-session -s thanhle -n app -d
 [ ! -z "$(tmux has-session -t thanhle:app 2>&1)" ] && {
