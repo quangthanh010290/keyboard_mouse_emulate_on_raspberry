948725983760ff19d9096bc3163ea8fbdd36b855
Raraph84 <contact@raraph.fr>
2024-08-25 20:09:39 +0200

    Ignore missing packages in setup to avoir breaking the whole installation if one package is deleted in the future

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install -y git tmux bluez bluez-tools bluez-firmware
-sudo apt-get install -y python3 python3-dev python3-pip python3-dbus python3-pyudev python3-evdev python3-gi
+sudo apt-get install -y --ignore-missing git tmux bluez bluez-tools bluez-firmware
+sudo apt-get install -y --ignore-missing python3 python3-dev python3-pip python3-dbus python3-pyudev python3-evdev python3-gi
 
 sudo apt-get install -y libbluetooth-dev
 sudo pip3 install PyBluez
