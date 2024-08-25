07337478563f6d55264446009e01a46d3f5ef3a9
Raraph84 <contact@raraph.fr>
2024-08-25 19:59:23 +0200

    Reorganize setup.sh

--- setup.sh
+++ setup.sh
@@ -2,15 +2,18 @@
 sudo apt-get update -y
 sudo apt-get install bluez bluez-tools -y
 sudo apt-get install bluez-firmware python-bluez python-dev python-pip -y
-sudo pip install evdev
 sudo apt install git python python3 python-dev python3-dev python3-dbus python3-pyudev python3-evdev -y
 sudo apt-get install python-dbus  -y
 sudo apt-get install tmux -y
+sudo apt-get install python3-gi -y
+sudo apt install python3-pip libbluetooth-dev -y
+
+sudo pip3 install PyBluez
+sudo pip install evdev
+
 sudo cp dbus/org.thanhle.btkbservice.conf /etc/dbus-1/system.d
+
 sudo cp /lib/systemd/system/bluetooth.service ./bluetooth.service.bk
 sudo cp bluetooth.service /lib/systemd/system/bluetooth.service
 sudo systemctl daemon-reload
 sudo /etc/init.d/bluetooth start
-sudo apt-get install python3-gi -y
-sudo apt install python3-pip libbluetooth-dev -y
-sudo pip3 install PyBluez
