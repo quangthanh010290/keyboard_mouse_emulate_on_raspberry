66c29c7f5516a76c516e5e197807e71a0e9909bb
Raraph84 <contact@raraph.fr>
2024-09-01 17:41:57 +0200

    Use sed to edit bluetooth service instead of replacing file, install pybluez from source and restart dbus and bluetooth services to fix install on bookworm

--- setup.sh
+++ setup.sh
@@ -4,11 +4,11 @@ sudo apt-get install -y --ignore-missing git tmux bluez bluez-tools bluez-firmwa
 sudo apt-get install -y --ignore-missing python3 python3-dev python3-pip python3-dbus python3-pyudev python3-evdev python3-gi
 
 sudo apt-get install -y libbluetooth-dev
-sudo pip3 install PyBluez
+sudo pip3 install git+https://github.com/pybluez/pybluez.git#egg=pybluez --break-system-packages
 
 sudo cp dbus/org.thanhle.btkbservice.conf /etc/dbus-1/system.d
+sudo systemctl restart dbus.service
 
-sudo cp /lib/systemd/system/bluetooth.service ./bluetooth.service.bk
-sudo cp bluetooth.service /lib/systemd/system/bluetooth.service
+sudo sed -i '/^ExecStart=/ s/$/ --noplugin=input/' /lib/systemd/system/bluetooth.service
 sudo systemctl daemon-reload
-sudo /etc/init.d/bluetooth start
+sudo systemctl restart bluetooth.service
