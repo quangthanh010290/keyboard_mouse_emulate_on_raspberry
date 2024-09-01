cfab6f5939caeae37aba2146ba0c7978c998cb33
Raraph84 <contact@raraph.fr>
2024-09-01 21:47:51 +0200

    Use PIP_BREAK_SYSTEM_PACKAGES=1 instead of --break-system-packages to not break setup on bullseye

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt-get install -y --ignore-missing git tmux bluez bluez-tools bluez-firmwa
 sudo apt-get install -y --ignore-missing python3 python3-dev python3-pip python3-dbus python3-pyudev python3-evdev python3-gi
 
 sudo apt-get install -y libbluetooth-dev
-sudo pip3 install git+https://github.com/pybluez/pybluez.git#egg=pybluez --break-system-packages
+sudo PIP_BREAK_SYSTEM_PACKAGES=1 pip3 install git+https://github.com/pybluez/pybluez.git#egg=pybluez
 
 sudo cp dbus/org.thanhle.btkbservice.conf /etc/dbus-1/system.d
 sudo systemctl restart dbus.service
